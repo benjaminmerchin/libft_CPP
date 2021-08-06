47722bca42010ece5c4af012eed79d33a471a038
benjaminmerchin <benjaminmerchin@gmail.com>
2021-08-06 21:07:15 +0200

    oui

--- base_to_int.cpp
+++ base_to_int.cpp
@@ -3,13 +3,13 @@
 int base_to_int(std::string str) {
     std::string base = "0123456789ABCDEF";
     int len = base.length();
-    int val = len, tot = 0;
+    int val = 1, tot = 0;
     for (int i = str.length()-1; i >= 0; i--) {
         for (int j = 0; base[j]; j++) {
             if (base[j] == str[i])
                 tot += val * j;
         }
-        val += len;
+        val *= len;
     }
     return tot;
 }
